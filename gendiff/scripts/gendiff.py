d2647ea6a940aada3a2010704c9fc53f3517c19b
Yuriy Totyshev <freewan@desktop>
2023-06-10 17:49:10 +0500

    add new argument -f (format)

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,8 +4,11 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
-    parser.add_argument('first_file', type=str, help='The first file to compare')
-    parser.add_argument('second_file', type=str, help='The second file to compare')
+    parser.add_argument('first_file', type=str, help='the path of the first file to compare')
+    parser.add_argument('second_file', type=str, help='the path of the second file to compare')
+    parser.add_argument('-f', '--format', 
+                        type=str, default='json', 
+                        help='set format of output. Can be plain, json (default) or stylish.')
 
     args = parser.parse_args()
 
