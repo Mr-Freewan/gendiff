5fa57282ea18d1cd030bdf512eaa3913b22a0f10
Yuriy Totyshev <freewan@desktop>
2023-06-15 03:30:43 +0500

    update: tests for sys exit and plain format

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -10,11 +10,12 @@ def get_path(file):
 
 @pytest.mark.parametrize(('file1_path', 'file2_path', 'formatter', 'expected'),
                          [  # Flat structure and stylish output
+                            # JSON
                             ('file1_flat.json',
                              'file2_flat.json',
                              'stylish',
                              'correct_flat_stylish_output.txt'),
-
+                            # YAML
                             ('file1_flat.yml',
                              'file2_flat.yaml',
                              'stylish',
@@ -24,12 +25,18 @@ def get_path(file):
                              'file2_flat.json',
                              'json',
                              'correct_flat_json_output.txt'),
+                            # Flat structure and plain output
+                            ('file1_flat.json',
+                             'file2_flat.json',
+                             'plain',
+                             'correct_flat_plain_output.txt'),
                             # Nested structure and stylish output
+                            # JSON
                             ('file1_nested.json',
                              'file2_nested.json',
                              'stylish',
                              'correct_nested_stylish_output.txt'),
-
+                            # YAML
                             ('file1_nested.yaml',
                              'file2_nested.yml',
                              'stylish',
@@ -39,6 +46,11 @@ def get_path(file):
                              'file2_nested.yml',
                              'json',
                              'correct_nested_json_output.txt'),
+                            # Nested structure and plain output
+                            ('file1_nested.yaml',
+                             'file2_nested.yml',
+                             'plain',
+                             'correct_nested_plain_output.txt'),
                             # Equal Files
                             ('file1_nested.json',
                              'file1_nested.json',
@@ -55,20 +67,21 @@ def test_generate_diff(file1_path: str, file2_path: str,
         assert generate_diff(test_path1, test_path2, formatter) == result_data
 
 
-@pytest.mark.parametrize(('file1_path', 'file2_path', 'formatter', 'expected'),
+@pytest.mark.parametrize(('file1_path', 'file2_path', 'formatter'),
                          [  # Wrong extension
                             ('file1_nested.txt',
                              'file2_nested.json',
-                             'stylish',
-                             None),
+                             'stylish'),
                             # File not exists
                             ('file1.json',
                              'file2_nested.json',
-                             'stylish',
-                             None),
+                             'stylish'),
                          ])
-def test_generate_diff_wrong_files(file1_path: str, file2_path: str,
-                                   formatter: str, expected: str) -> None:
+def test_wrong_files_sys_exit(file1_path: str, file2_path: str,
+                                   formatter: str) -> None:
     test_path1 = get_path(file1_path)
     test_path2 = get_path(file2_path)
-    assert generate_diff(test_path1, test_path2, formatter) == expected
+    with pytest.raises(SystemExit) as sys_exit:
+        generate_diff(test_path1, test_path2, formatter)
+    assert sys_exit.type == SystemExit
+    assert sys_exit.value.code == 1
